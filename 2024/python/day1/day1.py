9165ec97e41b328d0708842caa71eb8ca6428e25
Ishaan Subhash <isubhas@ncsu.edu>
2024-12-01 15:03:39 -0500

    Day 1 Python Solution

---
+++ 2024/python/day1/day1.py
@@ -0,0 +1,29 @@
+import bisect
+
+# PART 1
+leftList = []
+rightList = []
+distanceList = []
+
+# read ids into left and right list
+with open('C:\\Users\\Ishaan\\myprojects\\adventofcode\\2024\\python\\day1\\data_a.txt', 'r') as file:
+    for line in file:
+        words = line.split()
+        bisect.insort(rightList, int(words[1]))
+        bisect.insort(leftList, int(words[0]))
+
+# compute distance
+for i in range(len(leftList)):
+    distance = abs(leftList[i] - rightList[i])
+    distanceList.append(distance)
+    
+print(sum(distanceList))
+
+# PART 2
+similarityScore = 0
+
+# calculate similarity score
+for num in leftList:
+    similarityScore += num * rightList.count(num)
+
+print(similarityScore)
